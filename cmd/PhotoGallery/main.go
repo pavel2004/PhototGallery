ad65b66fbb7f53fb35b7e666e79b0c3d04087068
pavel2004 <p.sharygin@innopolis.university>
2025-05-12 15:23:59 +0300

    create logger and initialize it

--- cmd/PhotoGallery/main.go
+++ cmd/PhotoGallery/main.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"PhotoGallery/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
 )
 
 func main() {
 	cfg := config.MustConfig()
-	// TODO: init logger
-
+	log := setupLogger(cfg.Env)
+	log.Info("photo gallery start", slog.String("env", cfg.Env))
 	// TODO: init storage
 
 	// TODO: init router
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+
+	}
+	return log
 }
